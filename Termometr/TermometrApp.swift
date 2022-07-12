90cdfd81213ef459f76140ecea7e814561741153
Andrii Kyrychenko <akyrych84@gmail.com>
2022-07-12 16:07:03 -0400

    Initial Commit

---
+++ Termometr/TermometrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermometrApp.swift
+//  Termometr
+//
+//  Created by Andrii Kyrychenko on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TermometrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
